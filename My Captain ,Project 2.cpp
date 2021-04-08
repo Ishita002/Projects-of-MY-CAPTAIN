029f38c08d9a2f85e685ab2f25c3cbb4f3a72f4c
Ishita002 <82046044+Ishita002@users.noreply.github.com>
2021-04-08 17:16:37 +0530

    Add files via upload

---
+++ My Captain ,Project 2.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	double FirstNumber,SecondNumber,Productoftwonumbers;
+	cout<<"Enter any two numbers=";
+	cin>>FirstNumber>>SecondNumber;
+	Productoftwonumbers=FirstNumber*SecondNumber;
+	cout<<"Product ="<<Productoftwonumbers;
+	return 0;
+}
---
+++ My Captain,Project 1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"size of int: "<<sizeof(int)<<" bytes"<<endl;
+	cout<<"size of char: "<<sizeof(char)<<" bytes"<<endl;
+	cout<<"size of float: "<<sizeof(float)<<" bytes"<<endl;
+	cout<<"size of double: "<<sizeof(double)<<" bytes"<<endl;
+	return 0;
+}
